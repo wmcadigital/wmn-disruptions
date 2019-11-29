a24e333c43ec9c904856b959c6b8c66faa9a8e16
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-11-29 19:39:13 +0000

    project update / maps

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Tray from './Components/Tray/Tray';
+import Map from './Components/Map/Map';
 
 function App() {
   return (
     <div className="App">
+        <Map />
         <Header />
         <Tray />
     </div>
