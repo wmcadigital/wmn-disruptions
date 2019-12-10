44312fcd079990c623af61a915c48dc73e3f295b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-12-10 13:55:26 +0000

    Lint fixing

--- src/Components/Tray/Mode/Mode.js
+++ src/Components/Tray/Mode/Mode.js
@@ -11,64 +11,52 @@ import * as a from './actions';
 import * as searchActions from '../Search/actions';
 
 class Mode extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.ChangeModeToCheck = this.ChangeModeToCheck.bind(this);
-    }
+    this.ChangeModeToCheck = this.ChangeModeToCheck.bind(this);
+  }
 
-    ChangeModeToCheck(mode) {
-        const { props } = this;
-        const {
-            SetModeAction,
-            ResetSearch,
-            DeselectResult
-        } = props || {};
+  ChangeModeToCheck(mode) {
+    const { props } = this;
+    const { SetModeAction, ResetSearch, DeselectResult } = props || {};
 
-        DeselectResult();
-        ResetSearch();
-        SetModeAction(mode);
-    }
+    DeselectResult();
+    ResetSearch();
+    SetModeAction(mode);
+  }
 
-    render() {
-        const { ChangeModeToCheck } = this;
-        return (
-            <ModeView
-                changeModeToCheck={ChangeModeToCheck}
-            />
-        )
-    }
+  render() {
+    const { ChangeModeToCheck } = this;
+    return <ModeView changeModeToCheck={ChangeModeToCheck} />;
+  }
 }
 
 Mode.propTypes = {
-    modeToCheck: PropTypes.string,
+  modeToCheck: PropTypes.string
 };
 
 Mode.defaultProps = {
-    modeToCheck: '',
+  modeToCheck: ''
 };
 
 const mapStateToProps = state => {
-    const { mode } = state || {};
-    const { modeToCheck } = mode || {};
+  const { mode } = state || {};
+  const { modeToCheck } = mode || {};
 
-    return {
-        modeToCheck: modeToCheck || '',
-    }
-}
+  return {
+    modeToCheck: modeToCheck || ''
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    const { SetModeAction } = a || {};
-    const { ResetSearch, DeselectResult } = searchActions || {};
-    return {
-        SetModeAction: data => dispatch(SetModeAction(data)),
-        ResetSearch: data => dispatch(ResetSearch(data)),
-        DeselectResult: data => dispatch(DeselectResult(data))
-    }
-}
+  const { SetModeAction } = a || {};
+  const { ResetSearch, DeselectResult } = searchActions || {};
+  return {
+    SetModeAction: data => dispatch(SetModeAction(data)),
+    ResetSearch: data => dispatch(ResetSearch(data)),
+    DeselectResult: data => dispatch(DeselectResult(data))
+  };
+};
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Mode);
-
+export default connect(mapStateToProps, mapDispatchToProps)(Mode);
