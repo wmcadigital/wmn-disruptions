562a872b7e7f4b7e5d111b25cfb20a00ce3f22c2
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-12-12 11:46:17 +0000

    Added comments to button component

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -9,26 +9,29 @@ const Button = ({ type, isActive, text, onClick, btnClass, iconLeft, iconRight }
     <button
       type={type}
       className={`wmnds-btn ${btnClass} ${isActive ? 'wmnds-is--active' : null}`}
-      tabIndex="0"
       onClick={e => onClick(e)}
     >
+      {/* If icon left is set then call icon component and inject correct svg */}
       {iconLeft ? <Icon iconClass="wmnds-btn__icon" iconName={iconLeft} /> : null}
 
+      {/* button text will go here, if any */}
       {text}
 
+      {/* If icon right is set then call icon component and inject correct svg */}
       {iconRight ? <Icon iconClass="wmnds-btn__icon wmnds-btn__icon--right" iconName={iconRight} /> : null}
     </button>
   );
 };
 
+// Set props
 Button.propTypes = {
-  text: PropTypes.string,
-  type: PropTypes.string,
-  onClick: PropTypes.func,
-  isActive: PropTypes.bool,
-  btnClass: PropTypes.string,
-  iconLeft: PropTypes.string,
-  iconRight: PropTypes.string
+  text: PropTypes.string, // text inside button
+  type: PropTypes.string, // button type, by default it is type="button"
+  onClick: PropTypes.func, // Set an onclick event
+  isActive: PropTypes.bool, // If button is active, add active class
+  btnClass: PropTypes.string, // Set custom button classes, will default to wmnds-btn (primary btn)
+  iconLeft: PropTypes.string, // Set icon left on button
+  iconRight: PropTypes.string // Set icon right on button
 };
 
 Button.defaultProps = {
