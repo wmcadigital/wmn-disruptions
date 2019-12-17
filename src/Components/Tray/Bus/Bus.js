d61705cb560b5381242bb2ae558fc56ce6330a58
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-12-17 10:04:46 +0000

    Updated API call so it now takes a query

--- src/Components/Tray/Bus/Bus.js
+++ src/Components/Tray/Bus/Bus.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BusInfo from './BusInfo';
 import NewSearch from '../../Search/NewSearch';
 
-function Bus() {
-  return (
-    <div>
-      <h6>Bus Info</h6>
-      <NewSearch />
-      <BusInfo />
-    </div>
-  );
-}
+export default class Bus extends Component {
+  constructor() {
+    super();
 
-export default Bus;
+    this.updateQuery = this.updateQuery.bind(this);
+
+    this.state = {
+      query: ''
+    };
+  }
+
+  updateQuery(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  render() {
+    const { query } = this.state;
+    return (
+      <div>
+        <br />
+        <NewSearch query={query} updateQuery={this.updateQuery} />
+        <BusInfo query={query} />
+      </div>
+    );
+  }
+}
