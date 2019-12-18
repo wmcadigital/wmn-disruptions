f775c9523f35a8837eb627909fda60c30d2ead4f
Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
2019-12-18 16:19:09 +0000

    Ds map (#80)
    * Started if else statement on bus disruption
    
    * Updated busInfo to now have correct disruption indicators
    
    * Updated disruption indicator to include help text

--- src/Components/Tray/Bus/BusInfo.js
+++ src/Components/Tray/Bus/BusInfo.js
@@ -41,37 +41,68 @@ class BusInfo extends Component {
 
     return (
       <ul className={s.results}>
-        {data.map(el => (
-          <li className="wmnds-grid" key={el.id}>
-            <div className={`${s.indicator} wmnds-col-auto`}>
-              {el.hasDisruptions ? (
-                // Has disruptions
-                <div className="wmnds-disruption-indicator-medium wmnds-disruption-indicator-medium--with-icon wmnds-disruption-indicator-medium--warning">
+        {data.map(el => {
+          let type;
+          let iconName;
+          let text;
+          // If the current service has disruption
+          if (el.hasDisruptions) {
+            // Do a switch on the disruption severity, then map the type and iconName to the correct vars
+            switch (el.disruptionSeverity) {
+              // Minor disruption (normal)
+              case 'normal':
+                type = 'warning';
+                iconName = 'warning-circle';
+                text = 'Minor disruption';
+                break;
+              // Major disruption (high)
+              case 'high':
+                type = 'error';
+                iconName = 'warning-triangle';
+                text = 'Major disruption';
+                break;
+              // Severe disruption (veryHigh)
+              case 'veryHigh':
+                type = 'severe';
+                iconName = 'warning-triangle';
+                text = 'Severe disruption';
+                break;
+              // Good service (low)
+              default:
+                type = 'success';
+                iconName = 'success';
+                text = 'Good service';
+                break;
+            }
+          } else {
+            // No disruptions, so show success
+            type = 'success';
+            iconName = 'success';
+            text = 'Good service';
+          }
+          // Return service with the above disruption logic, replace type and iconName with correc icon and class depending on disruption type
+          return (
+            <li className="wmnds-grid" key={el.id} title={`${text} on ${el.serviceNumber}`}>
+              <div className={`${s.indicator} wmnds-col-auto`}>
+                <div
+                  className={`wmnds-disruption-indicator-medium wmnds-disruption-indicator-medium--with-icon wmnds-disruption-indicator-medium--${type}`}
+                >
                   {el.serviceNumber}
                   <Icon
                     iconClass="wmnds-disruption-indicator-medium__icon wmnds-disruption-indicator-medium__icon--right"
-                    iconName="general-warning-circle"
+                    iconName={`general-${iconName}`}
                   />
                 </div>
-              ) : (
-                // No disruptions
-                <div className="wmnds-disruption-indicator-medium wmnds-disruption-indicator-medium--with-icon wmnds-disruption-indicator-medium--success">
-                  {el.serviceNumber}
-                  <Icon
-                    iconClass="wmnds-disruption-indicator-medium__icon wmnds-disruption-indicator-medium__icon--right"
-                    iconName="general-success"
-                  />
-                </div>
-              )}
-            </div>
-            {/* Right section */}
-            <div className="wmnds-col-1-2 wmnds-col-sm-2-3">
-              <strong>{el.routes[0].routeName}</strong>
-              <br />
-              and return journey
-            </div>
-          </li>
-        ))}
+              </div>
+              {/* Right section */}
+              <div className="wmnds-col-1-2 wmnds-col-sm-2-3">
+                <strong>{el.routes[0].routeName}</strong>
+                <br />
+                and return journey
+              </div>
+            </li>
+          );
+        })}
       </ul>
     );
   }
