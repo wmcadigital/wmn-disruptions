ea2488bb111b4dd8df61c74e8f01f18af937fad2
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-12-18 11:48:48 +0000

    Update map base layer to correct one.  Closes #75.

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -9,32 +9,33 @@ export default class WebMapView extends React.Component {
 
   componentDidMount() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/VectorTileLayer'], {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/Basemap', 'esri/layers/VectorTileLayer'], {
       css: true
-    }).then(([Map, MapView, VectorTileLayer]) => {
-      // eslint-disable-next-line no-param-reassign
+    }).then(([Map, MapView, Basemap, VectorTileLayer]) => {
+      // When loaded, create a new basemap
+      const basemap = new Basemap({
+        baseLayers: [
+          new VectorTileLayer({
+            portalItem: {
+              // set the basemap to the one being used: https://tfwm.maps.arcgis.com/home/item.html?id=53f165a8863c4d40ba017042e248355e
+              id: '53f165a8863c4d40ba017042e248355e'
+            }
+          })
+        ]
+      });
 
-      // const map = new WebMap({
-      //   portalItem: {
-      //     // autocasts as new PortalItem()
-      //     id: '53f165a8863c4d40ba017042e248355e'
-      //   }
-      // });
-      const map = new Map();
+      // Create a new map with the basemap set above
+      const map = new Map({
+        basemap
+      });
 
+      // Create a new map view with settings
       this.view = new MapView({
         container: this.mapRef.current,
         map,
         center: [-2.0047209, 52.4778132],
         zoom: 10
       });
-
-      // The URL referenced in the constructor may point to a style url JSON (as in this sample)
-      // or directly to a vector tile service
-      const vtlayer = new VectorTileLayer({
-        url: 'https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer'
-      });
-      map.add(vtlayer);
     });
   }
 
