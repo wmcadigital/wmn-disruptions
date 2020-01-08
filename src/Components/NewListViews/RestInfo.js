b28644c7194cf9a1a54bcc7c685ac5a2544077bb
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-01-08 07:32:20 +0000

    Updated list view and adding RestInfo - app now loads.

--- src/Components/NewListViews/RestInfo.js
+++ src/Components/NewListViews/RestInfo.js
@@ -1,37 +1,11 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export class RestInfo extends Component {
-  constructor() {
-    super();
-    state = {
-      data: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get('https://trasnport-api-isruptions-v2.azure-api.net/bus/v1/service', {
-      headers: {
-        'Ocp-Apim-Subscription-Key': '55060e2bfbf743c5829b9eef583506f7'
-      }.then(res => {
-        const data = res.data;
-        this.setState({ data });
-      })
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello Data</h1>
-        <ul>
-          {this.state.data.map(data => (
-            <li>data.disruptions</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function RestInfo() {
+  return (
+    <div>
+      <h1>The Disruptions</h1>
+    </div>
+  );
 }
 
 export default RestInfo;
