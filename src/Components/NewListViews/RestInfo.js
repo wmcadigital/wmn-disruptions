f7e535344cd0d25ca09e947d01450edc1d2d3bc0
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-01-08 10:02:21 +0000

    API data and removed single bus old

--- src/Components/NewListViews/RestInfo.js
+++ src/Components/NewListViews/RestInfo.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function RestInfo() {
-  return (
-    <div>
-      <h1>The Disruptions</h1>
-    </div>
-  );
+class RestInfo extends Component {
+  componentDidMount() {
+    axios
+      .get('https://trasnport-api-isruptions-v2.azure-api.net/Disruption/v2/', {
+        headers: {
+          'Ocp-Apim-Subscription-Key': '55060e2bfbf743c5829b9eef583506f7'
+        }
+      })
+      .then(response => {
+        console.log(`this is the ${response}`);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Disruptions</h1>
+      </div>
+    );
+  }
 }
 
 export default RestInfo;
