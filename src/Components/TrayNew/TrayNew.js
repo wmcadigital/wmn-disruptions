640dd70bc8cd488acab772397174eb5f86254f59
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-01-24 14:48:38 +0000

    Split datepicker to new component

--- src/Components/TrayNew/TrayNew.js
+++ src/Components/TrayNew/TrayNew.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
 
 // Import components
 import Button from '../Button/Button';
+import NewDatePicker from './DatePickerNew/DatePickerNew';
 
 // Import styles
 import s from './TrayNew.module.scss';
 
 const TrayNew = props => {
-  const { setWhen, when, datePicker } = props || {};
+  const { setWhen, when } = props || {};
 
   const today = new Date(); // Get today's date
   const nowText = `Now ${today.getHours()}:${today.getMinutes()}`; // Set nowText to be 'Now HH:MM'
@@ -40,19 +40,7 @@ const TrayNew = props => {
         />
         <div className="wmnds-col-auto">
           {/* Choose date button */}
-          <div className={s.chooseDateWrapper}>
-            <Button
-              btnClass="wmnds-btn--secondary wmnds-btn--small wmnds-p-xsm"
-              isActive={when === 'customDate'}
-              onClick={() => setWhen('customDate')}
-              className={`${s.btn} ${s.chooseDateBtn}`}
-              text="Choose date"
-            />
-
-            <span className={s.datePicker} style={{ display: 'none' }}>
-              <DatePicker selected={today} onSelect={datePicker} withPortal />
-            </span>
-          </div>
+          <NewDatePicker today={today} when={when} setWhen={setWhen} />
         </div>
       </div>
     </div>
