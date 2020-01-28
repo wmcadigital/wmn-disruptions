e351f4f04f1998d8c4f2a87a2e3ff6fdc7aa2122
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-01-28 10:38:29 +0000

    Updated logic so that bus autocomplete works correctly

--- src/Components/TrayNew/AutoComplete/Bus/BusAutoComplete.js
+++ src/Components/TrayNew/AutoComplete/Bus/BusAutoComplete.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { DebounceInput } from 'react-debounce-input'; // https://www.npmjs.com/package/react-debounce-input
 
 // Import contexts
 import { AutoCompleteContext } from '../AutoCompleteContext';
@@ -10,9 +11,11 @@ import BusAutoCompleteResult from './BusAutoCompleteResult';
 
 const BusAutoComplete = () => {
   const [autoCompleteState, autoCompleteDispatch] = useContext(AutoCompleteContext); // Get the state of modeButtons from modeContext
+  const [loading, setLoading] = useState(); // set state for loading spinner
 
   useEffect(() => {
     if (autoCompleteState.query) {
+      setLoading(true); // Update loading state to true as we are hitting API
       axios
         .get(`https://trasnport-api-isruptions-v2.azure-api.net/bus/v1/service?q=${autoCompleteState.query}`, {
           headers: {
@@ -20,19 +23,20 @@ const BusAutoComplete = () => {
           }
         })
         .then(bus => {
-          autoCompleteDispatch({ type: 'UPDATE_DATA', data: bus.data.services });
+          autoCompleteDispatch({ type: 'UPDATE_DATA', data: bus.data.services }); // Update data state with services returned
+          setLoading(false); // Set loading state to false after data is received
         });
     }
-  }, [autoCompleteDispatch, autoCompleteState.query]);
+  }, [autoCompleteDispatch, autoCompleteState.data.length, autoCompleteState.query]);
 
   return (
     <>
-      <div className="wmnds-autocomplete wmnds-grid">
+      <div className={`wmnds-autocomplete wmnds-grid ${loading ? 'wmnds-is--loading' : ''}`}>
         <Icon iconName="general-search" iconClass="wmnds-autocomplete__icon" />
         <div className="wmnds-loader" role="alert" aria-live="assertive">
           <p className="wmnds-loader__content">Content is loading...</p>
         </div>
-        <input
+        <DebounceInput
           type="text"
           name="busSearch"
           placeholder="Search for a service"
@@ -40,13 +44,26 @@ const BusAutoComplete = () => {
           value={autoCompleteState.query}
           onChange={e => autoCompleteDispatch({ type: 'UPDATE_QUERY', query: e.target.value })}
           aria-label="Search for a service"
+          debounceTimeout={600}
         />
       </div>
-      <ul className="wmnds-autocomplete-suggestions">
-        {autoCompleteState.data.map(result => (
-          <BusAutoCompleteResult key={result.id} result={result} />
-        ))}
-      </ul>
+      {/* If there is no data.length(results) and the user hasn't submitted a query and the state isn't loading then the user should be displayed with no results message, else show results */}
+      {!autoCompleteState.data.length && autoCompleteState.query && !loading ? (
+        <p className="wmnds-col-1 wmnds-m-t-sm">
+          {'Oops! Sorry, no results have been found for '}
+          <strong>{autoCompleteState.query}</strong>
+          {'. '}
+          <br />
+          <br />
+          Please try searching for another service.
+        </p>
+      ) : (
+        <ul className="wmnds-autocomplete-suggestions">
+          {autoCompleteState.data.map(result => (
+            <BusAutoCompleteResult key={result.id} result={result} />
+          ))}
+        </ul>
+      )}
     </>
   );
 };
