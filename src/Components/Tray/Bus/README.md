60c2ba929b502c4f1bf57ed2a3db3ff39707329a
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-01-31 11:25:59 +0000

    Deleted old redux, app.js, and tray components.

--- src/Components/Tray/Bus/README.md
+++
@@ -1,3 +0,0 @@
-#Bus Filter
-
-Real API data is now working with the search component for Bus only.
--- src/Components/Tray/Mode/actions.js
+++
@@ -1,5 +0,0 @@
-export const SET_MODE = 'set mode';
-
-export function SetModeAction(data) {
-  return { type: SET_MODE, payload: data };
-}
