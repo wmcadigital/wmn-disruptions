0479ac790be967de0642b6679764f81b59f35028
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-02-06 09:51:41 +0000

    Mode filtering is now workign

--- src/components/ListView/DisruptionList/DisruptionList.js
+++ src/components/ListView/DisruptionList/DisruptionList.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
+// Import contexts
+import { ModeContext } from 'globalState';
 import DisruptionItem from './DisruptionItem/DisruptionItem';
 
 
 const DisruptionList = () => {
+  const [modeState] = useContext(ModeContext); // Get the state of whenButtons from WhenContext
   const [data, setdata] = useState([]);
   const [isFetching, setisFetching] = useState(false);
 
@@ -17,25 +20,35 @@ const DisruptionList = () => {
       })
       .then(response => {
         setdata(response.data.disruptions);
-        setisFetching(false);
-      });
-  }, [])
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      })
+      .then(() => setisFetching(false));
+  }, []);
 
-return(
-  <>
-    { !isFetching ?
-      data.map(disruption => (
-        <DisruptionItem disruption={disruption} />
-      ))
-      :
-      (
+// Mode filtering
+  let filteredData = data;
+  if(modeState.mode){
+    filteredData = filteredData.filter(disrItem => disrItem.mode === modeState.mode);
+  }
+
+  return (
+    <>
+      {!isFetching ? (
+        filteredData.map(disruption => <DisruptionItem disruption={disruption} />)
+      ) : (
         <div>
           <div className="wmnds-loader" />
         </div>
-      )
-    }
-  </>
-)
-}
+      )}
+    </>
+  );
+};
 
 export default DisruptionList
