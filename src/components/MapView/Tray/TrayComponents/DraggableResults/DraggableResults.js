919a63b873286b18b2c6fc787f9f189404e73241
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-06 14:52:51 +0000

    updated the styles

--- src/components/MapView/Tray/TrayComponents/DraggableResults/DraggableResults.js
+++ src/components/MapView/Tray/TrayComponents/DraggableResults/DraggableResults.js
@@ -15,9 +15,7 @@ function DraggableResults() {
           </svg>
         </span>
         <h3>Broken Down Van at M20 </h3>
-
         <h3>Road(s) affected:</h3>
-
         <div className="wmnds-disruption-indicator-large wmnds-disruption-indicator-large--warning">
           <div className="wmnds-disruption-indicator-large__left-wrapper">
             <span className="wmnds-disruption-indicator-large__left-icon-wrapper">
@@ -38,18 +36,28 @@ function DraggableResults() {
             <Icon iconName="general-star" iconClass="general-star" />
           </svg>
         </div>
-
         <h5>Expect to continue until</h5>
         <p>Friday 19th October 2020 at 5:00am</p>
-
         <h5>Whats Happened</h5>
         <p>Broken down van. Traffic Officers and Free Recovery are headed towards. Approach with caution. </p>
-
         <h5>Travel Advice</h5>
-
         <p>Lorem Ipsum is the root of goodness. Dummy test is printing and typesetting industry. </p>
-
         <h5>Latest updates (#HashTag on Twitter)</h5>
+        <button className="wmnds-btn wmnds-btn--start" type="button">
+          Replan Your Journey
+          <svg className="wmnds-btn__icon wmnds-btn__icon--right">
+            <Icon iconName="general-chevron-right" iconClass="general-chevron-right" />
+          </svg>
+        </button>
+        <br />
+        <br />
+
+        <button className="wmnds-btn" type="button">
+          Share Disruption
+          <svg className="wmnds-btn__icon wmnds-btn__icon--right">
+            <Icon iconName="general-share" iconClass="general-share" />
+          </svg>
+        </button>
       </div>
     </div>
   );
