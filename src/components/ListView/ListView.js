85056ecd3795e2bbb8926d660c206dab8ea26f0f
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-02-21 09:11:37 +0000

    Removed listview.scss

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -3,7 +3,6 @@ import TrayComponents from 'components/shared/Tray/TrayComponents/TrayComponents
 // Import styles
 import trayStyle from 'components/shared/Tray/Tray.module.scss';
 import s from './ListView.module.scss';
-import './ListView.scss';
 // Import components
 import DisruptionList from './DisruptionList/DisruptionList';
 
@@ -12,7 +11,7 @@ function NewListView() {
     <div className="wmnds-container wmnds-p-t-md wmnds-p-b-md">
       <div className="wmnds-grid wmnds-grid--justify-between">
         <div className="wmnds-col-1 wmnds-col-md-1-3">
-          <div className={`${trayStyle.tray} wmnds-grid wmnds-p-md listview-tray`}>
+          <div className={`${trayStyle.tray} wmnds-grid wmnds-p-md ${s.tray}`}>
             <TrayComponents />
           </div>
         </div>
