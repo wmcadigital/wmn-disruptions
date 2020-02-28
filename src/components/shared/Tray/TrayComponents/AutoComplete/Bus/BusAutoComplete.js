f0af84507f1090420584bfdf27b6d3105e6fe465
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-02-28 12:06:42 +0000

    Autocomplete is now working correctly

--- src/components/shared/Tray/TrayComponents/AutoComplete/Bus/BusAutoComplete.js
+++ src/components/shared/Tray/TrayComponents/AutoComplete/Bus/BusAutoComplete.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { DebounceInput } from 'react-debounce-input'; // https://www.npmjs.com/package/react-debounce-input
 
@@ -11,15 +11,13 @@ import BusAutoCompleteResult from './BusAutoCompleteResult';
 
 const BusAutoComplete = () => {
   const [autoCompleteState, autoCompleteDispatch] = useContext(AutoCompleteContext); // Get the state of modeButtons from modeContext
-  const [loading, setLoading] = useState(); // set state for loading spinner
+  const loading = useRef(false); // Set loading ref for spinner (set as ref and not state to stop below useEffect re-rendering)
 
   useEffect(() => {
-    const { CancelToken } = axios; // Set up a cancelToken
-    const source = CancelToken.source(); // Set source of cancelToken
-
-    console.log({ query: autoCompleteState.query, loading });
+    const source = axios.CancelToken.source(); // Set source of cancelToken
+    // If autocomplete has query
     if (autoCompleteState.query) {
-      setLoading(true); // Update loading state to true as we are hitting API
+      loading.current = true; // Update loading state to true as we are hitting API
       axios
         .get(
           `https://trasnport-api-isruptions-v2.azure-api.net/bus/v1/service?q=${autoCompleteState.query}`,
@@ -44,7 +42,7 @@ const BusAutoComplete = () => {
           console.log(error);
         })
         .then(() => {
-          setLoading(false); // Set loading state to false after data is received
+          loading.current = false; // Set loading state to false after data is received
         });
     }
     // Unmount / cleanup
@@ -55,7 +53,9 @@ const BusAutoComplete = () => {
 
   return (
     <>
-      <div className={`wmnds-autocomplete wmnds-grid ${loading ? 'wmnds-is--loading' : ''}`}>
+      <div
+        className={`wmnds-autocomplete wmnds-grid ${loading.current ? 'wmnds-is--loading' : ''}`}
+      >
         <Icon iconName="general-search" iconClass="wmnds-autocomplete__icon" />
         <div className="wmnds-loader" role="alert" aria-live="assertive">
           <p className="wmnds-loader__content">Content is loading...</p>
@@ -68,11 +68,11 @@ const BusAutoComplete = () => {
           value={autoCompleteState.query}
           onChange={e => autoCompleteDispatch({ type: 'UPDATE_QUERY', query: e.target.value })}
           aria-label="Search for a service"
-          debounceTimeout={600}
+          // debounceTimeout={600}
         />
       </div>
       {/* If there is no data.length(results) and the user hasn't submitted a query and the state isn't loading then the user should be displayed with no results message, else show results */}
-      {!autoCompleteState.data.length && autoCompleteState.query && !loading ? (
+      {!autoCompleteState.data.length && autoCompleteState.query && !loading.current ? (
         <p className="wmnds-col-1 wmnds-m-t-sm">
           {'Oops! Sorry, no results have been found for '}
           <strong>{autoCompleteState.query}</strong>
