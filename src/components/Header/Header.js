68fe02877eabbafe7203adaddcd45f4c7882d0d9
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-20 14:24:37 +0000

    Moved breadcrumbs inside app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,22 +10,48 @@ const Header = ({ isMapVisible }) => {
   const [fetchDisruptionState, setFetchDisruptionsState] = useContext(FetchDisruptionsContext);
 
   return (
-    <div className="wmnds-container">
-      <div className="wmnds-grid wmnds-grid--justify-between wmnds-grid--align-middle wmnds-p-b-sm wmnds-p-t-sm">
-        <h1 className={`wmnds-col-auto wmnds-m-b-none ${s.h1}`}>Disruptions</h1>
+    <>
+      <nav
+        aria-label="Breadcrumb"
+        className="wmnds-breadcrumb wmnds-breadcrumb--mobile-app wmnds-container"
+      >
+        <ol className="wmnds-breadcrumb__list">
+          <li className="wmnds-breadcrumb__list-item">
+            <a href="//wmnetwork.co.uk" className="wmnds-breadcrumb__link">
+              Home
+            </a>
+          </li>
+          <li className="wmnds-breadcrumb__list-item">
+            <a
+              href="/components/breadcrumb/"
+              className="wmnds-breadcrumb__link wmnds-breadcrumb__link--current"
+              aria-current="page"
+            >
+              Disruptions
+            </a>
+          </li>
+        </ol>
+      </nav>
+      <div className="wmnds-container">
+        <div className="wmnds-grid wmnds-grid--justify-between wmnds-grid--align-middle wmnds-p-b-sm wmnds-p-t-sm">
+          <h1 className={`wmnds-col-auto wmnds-m-b-none ${s.h1}`}>Disruptions</h1>
 
-        <div className="wmnds-col-auto">
-          <Button
-            btnClass={`wmnds-btn--secondary wmnds-float--right ${s.listMapBtn}`}
-            onClick={() =>
-              setFetchDisruptionsState(prevState => ({ ...prevState, isMapVisible: !isMapVisible }))
-            }
-            iconRight="general-chevron-right"
-            text={fetchDisruptionState.isMapVisible ? 'List View' : 'Map View'}
-          />
+          <div className="wmnds-col-auto">
+            <Button
+              btnClass={`wmnds-btn--secondary wmnds-float--right ${s.listMapBtn}`}
+              onClick={() =>
+                setFetchDisruptionsState(prevState => ({
+                  ...prevState,
+                  isMapVisible: !isMapVisible
+                }))
+              }
+              iconRight="general-chevron-right"
+              text={fetchDisruptionState.isMapVisible ? 'List View' : 'Map View'}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
