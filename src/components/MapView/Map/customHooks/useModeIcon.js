66b2a454eca3cc0152e5140b8498cbe7fae4c7e9
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-23 15:50:01 +0000

    udpated notes to jpg

--- src/components/MapView/Map/customHooks/useModeIcon.js
+++ src/components/MapView/Map/customHooks/useModeIcon.js
@@ -1,19 +1,19 @@
 // // bus icons
-// import busMinor from 'assets/map-icons/bus-minor.png';
-// import busMajor from 'assets/map-icons/bus-major.png';
-// import busSevere from 'assets/map-icons/bus-severe.png';
+// import busMinor from 'assets/map-icons/bus-minor.jpg';
+// import busMajor from 'assets/map-icons/bus-major.jpg';
+// import busSevere from 'assets/map-icons/bus-severe.jpg';
 // // tram icons
-// import tramMinor from 'assets/map-icons/tram-minor.png';
-// import tramMajor from 'assets/map-icons/tram-major.png';
-// import tramSevere from 'assets/map-icons/tram-severe.png';
+// import tramMinor from 'assets/map-icons/tram-minor.jpg';
+// import tramMajor from 'assets/map-icons/tram-major.jpg';
+// import tramSevere from 'assets/map-icons/tram-severe.jpg';
 // // train icons
-// import trainMinor from 'assets/map-icons/train-minor.png';
-// import trainMajor from 'assets/map-icons/train-major.png';
-// import trainSevere from 'assets/map-icons/train-severe.png';
+// import trainMinor from 'assets/map-icons/train-minor.jpg';
+// import trainMajor from 'assets/map-icons/train-major.jpg';
+// import trainSevere from 'assets/map-icons/train-severe.jpg';
 // // roads icons
-// import roadsMinor from 'assets/map-icons/roads-minor.png';
-// import roadsMajor from 'assets/map-icons/roads-major.png';
-// import roadsSevere from 'assets/map-icons/roads-severe.png';
+// import roadsMinor from 'assets/map-icons/roads-minor.jpg';
+// import roadsMajor from 'assets/map-icons/roads-major.jpg';
+// import roadsSevere from 'assets/map-icons/roads-severe.jpg';
 
 const modeIcon = (mode, _severity, selected) => {
   let severity;
