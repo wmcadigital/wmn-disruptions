e48042cd7592bdbcda07b89e4d16bac909031e06
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-05-27 11:25:10 +0100

    tided up code

--- src/components/shared/Tray/MobileTray.js
+++ src/components/shared/Tray/MobileTray.js
@@ -10,62 +10,57 @@ import TrayComponents from './TrayComponents/TrayComponents';
 import s from './Tray.module.scss';
 
 const MobileTray = () => {
+  const [autoCompleteState] = useContext(AutoCompleteContext); // Get the state of modeButtons from modeContext
   const draggableTray = useRef(); // Ref used to keep track of Draggable dom element
   const slideableTray = useRef(); // Ref to track swipe dom element
-  const [autoCompleteState] = useContext(AutoCompleteContext); // Get the state of modeButtons from modeContext
   const { eleHeight } = useWindowHeightWidth(); // Get window height and width
-  const [position, setPosition] = useState(100); // Set initial position of tray
+  const initialTrayPosition = 100; // Initial position of tray
+  const secondThird = (eleHeight / 3) * 2; // Get the second third of the container height for tray to swipe to
+  const [trayPosition, setTrayPosition] = useState(initialTrayPosition); // Set initial position of tray
 
   // Open tray if there is a selectedMapDisruption (map icon has been clicked)
   useEffect(() => {
     if (autoCompleteState.selectedMapDisruption) {
-      setPosition(-eleHeight); // set tray to open
+      setTrayPosition(-eleHeight); // set tray to open
     }
   }, [autoCompleteState.selectedMapDisruption, eleHeight]);
 
-  /*
-  USED TO CONTROL INTERNAL SCROLLING OF TRAY (OVERFLOW SCROLL)
-  */
+  // SWIPE METHODS USED TO CONTROL SCROLLING OF TRAY
 
-  const secondThird = (eleHeight / 3) * 2; // Get the second third of the container
-
-  // OnStart function for start of swiping tray
   const onSwipeStart = () => {
     document.body.style.overflow = 'hidden'; // Set body overflow to hidden, so we don't snap to body scrollbar
   };
 
-  // onStop function for stop of swiping tray
   const onSwipeEnd = () => {
     document.body.style.overflow = null; // Scrolling finished so return body overflow to normal
   };
 
-  // On Swipe down
   const onSwipeDown = () => {
-    const trayScrollTop = slideableTray.current.swiper.scrollTop; // Get elementById so we can check the scollTop
-    // If tray is open and the scrollTop is 0 (we're at the top of the tray scroll), then unlock tray
+    const trayScrollTop = slideableTray.current.swiper.scrollTop; // Get DOM element, so we can check the scollTop
 
-    if (position === eleHeight && trayScrollTop === 0) setPosition(secondThird);
-    if (position === secondThird) setPosition(100);
+    if (trayPosition === eleHeight && trayScrollTop === 0) setTrayPosition(secondThird); // If tray is open(position===eleHeight) and the scrollTop is 0 (we're at the top of the tray scroll), then swipe down to secondThird position
+    if (trayPosition === secondThird) setTrayPosition(initialTrayPosition); // If tray is currently secondThird then swipe down to default position
   };
 
-  // On Swipe up
   const onSwipeUp = () => {
-    // If tray is open and the scrollTop is not 0 (we're not at the top of the tray scroll), so lock tray
-    if (position === 100) setPosition(secondThird);
-    if (position === secondThird) setPosition(eleHeight);
+    if (trayPosition === initialTrayPosition) setTrayPosition(secondThird); // If tray is initial position then swipe up to secondThird position
+    if (trayPosition === secondThird) setTrayPosition(eleHeight); // If tray is currently secondThird, then swipe up to full position
   };
 
   return (
     <div
       className={`${s.tray} wmnds-grid `}
+      // set height of tray to height of map/container, and set top position of tray based on logic above
       style={{
         height: `${eleHeight}px`,
-        top: typeof eleHeight !== 'number' ? '100%' : eleHeight - position,
+        top: typeof eleHeight !== 'number' ? '100%' : eleHeight - trayPosition,
       }}
       ref={draggableTray}
     >
       <Swipe
-        className={`${s.swipeTrayWrapper} wmnds-p-md ${position === eleHeight ? s.trayIsOpen : ''}`}
+        className={`${s.swipeTrayWrapper} wmnds-p-md ${
+          trayPosition === eleHeight ? s.trayIsOpen : ''
+        }`}
         onSwipeUp={onSwipeUp}
         onSwipeDown={onSwipeDown}
         onSwipeStart={onSwipeStart}
