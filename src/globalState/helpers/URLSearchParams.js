9e46282b3e72121231e88d2089c58c023eaba700
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-12 11:17:49 +0100

    Google tag manager now tracks correctly

--- src/globalState/helpers/URLSearchParams.js
+++ src/globalState/helpers/URLSearchParams.js
@@ -9,6 +9,16 @@ const getSearchParam = (name) => {
 const setSearchParam = (name, val) => {
   url.searchParams.set(name, val); // Set the search param with the name provided to the value provided
   window.history.pushState({}, '', url.href); // Then push the updated search params back to the URL
+
+  // If we ever set new search params then push the new URL to Google Analytics
+  window.dataLayer = window.dataLayer || []; // Set datalayer (GA thing)
+  // This is a Google Tag Manager event that allows to pass in a route
+  window.dataLayer.push({
+    event: 'ngRouteChange',
+    attributes: {
+      route: url.search,
+    },
+  });
 };
 
 // Function for deleting a search param in the URL
