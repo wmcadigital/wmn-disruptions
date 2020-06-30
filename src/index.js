91d9a0e7b1e975df44dfe2fc23ccfd7342a45862
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-30 14:53:14 +0100

    Updated sentry package to latest React one.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
 Sentry.init({ dsn: 'https://eec42d9779ba42eb84fa9623942bd149@o378798.ingest.sentry.io/5272889' });
 
-ReactDOM.render(<App />, document.getElementById('disruptionsApp'));
+ReactDOM.render(
+  <Sentry.ErrorBoundary>
+    <App />
+  </Sentry.ErrorBoundary>,
+  document.getElementById('disruptionsApp')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
