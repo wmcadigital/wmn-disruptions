e4f093731a1226137184f438ae22410216c5cdd8
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-07-09 14:36:39 +0100

    Started on animation of share buttons

--- src/components/shared/DisruptionInfo/ShareButtons/ShareButtons.js
+++ src/components/shared/DisruptionInfo/ShareButtons/ShareButtons.js
@@ -1,72 +1,73 @@
-import React from 'react';
-// External Components
+import React, { useState } from 'react';
 import {
   EmailIcon,
   EmailShareButton,
-  FacebookShareButton,
-  FacebookMessengerShareButton,
-  FacebookMessengerIcon,
-  InstapaperShareButton,
-  LineShareButton,
-  LinkedinShareButton,
-  LivejournalShareButton,
-  MailruShareButton,
-  OKShareButton,
-  PinterestShareButton,
-  PocketShareButton,
-  RedditShareButton,
-  TelegramShareButton,
-  TumblrShareButton,
-  TwitterShareButton,
-  ViberShareButton,
-  VKShareButton,
-  WhatsappShareButton,
-  WhatsappIcon,
-  WorkplaceShareButton,
-  TwitterIcon,
   FacebookIcon,
+  FacebookShareButton,
+  FacebookMessengerIcon,
+  FacebookMessengerShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
 } from 'react-share';
 // Components
 import PropTypes from 'prop-types';
 import Button from 'components/shared/Button/Button';
 // Import styles
 import s from '../DisruptionInfo.module.scss';
+import shareBtns from './ShareButtons.module.scss';
 
 const ShareButtons = ({ isMapVisible }) => {
+  const [showShareBtns, setShowShareBtns] = useState(false);
+
   return (
-    <>
+    <div className={shareBtns.mainWrapper}>
       <span className={`wmnds-col-1 ${isMapVisible ? s.mapBtn : `${s.listBtn} wmnds-col-sm-1-2`}`}>
-        <Button btnClass="wmnds-col-1" text="Share disruption" iconRight="general-share" />
+        <Button
+          btnClass="wmnds-col-1"
+          text="Share disruption"
+          iconRight="general-share"
+          onClick={() => setShowShareBtns(!showShareBtns)}
+        />
       </span>
-      <br />
-      <EmailShareButton className="wmnds-m-r-sm">
-        <EmailIcon size={45} />
-      </EmailShareButton>
 
-      <FacebookShareButton
-        className="wmnds-m-r-sm"
-        url={window.location.href}
-        hashtag="#wmn-disruptions"
-      >
-        <FacebookIcon size={45} />
-      </FacebookShareButton>
+      <div className={`${shareBtns.wrapper} ${showShareBtns ? `${shareBtns.active}` : ''}`}>
+        <EmailShareButton className="wmnds-m-r-sm">
+          <EmailIcon size={45} />
+        </EmailShareButton>
 
-      <FacebookMessengerShareButton
-        className="wmnds-m-r-sm"
-        url={window.location.href}
-        hashtag="#wmn-disruptions"
-      >
-        <FacebookMessengerIcon size={45} />
-      </FacebookMessengerShareButton>
+        <FacebookShareButton
+          className="wmnds-m-r-sm"
+          url={window.location.href}
+          hashtag="#wmn-disruptions"
+        >
+          <FacebookIcon size={45} />
+        </FacebookShareButton>
 
-      <TwitterShareButton className="wmnds-m-r-sm" url={window.location.href}>
-        <TwitterIcon size={45} />
-      </TwitterShareButton>
+        <FacebookMessengerShareButton
+          className="wmnds-m-r-sm"
+          url={window.location.href}
+          hashtag="#wmn-disruptions"
+        >
+          <FacebookMessengerIcon size={45} />
+        </FacebookMessengerShareButton>
 
-      <WhatsappShareButton className="wmnds-m-r-sm" url={window.location.href}>
-        <WhatsappIcon size={45} />
-      </WhatsappShareButton>
-    </>
+        <TwitterShareButton className="wmnds-m-r-sm" url={window.location.href}>
+          <TwitterIcon size={45} />
+        </TwitterShareButton>
+
+        <LinkedinShareButton className="wmnds-m-r-sm" url={window.location.href}>
+          <LinkedinIcon size={45} />
+        </LinkedinShareButton>
+
+        <WhatsappShareButton className="wmnds-m-r-sm" url={window.location.href}>
+          <WhatsappIcon size={45} />
+        </WhatsappShareButton>
+      </div>
+    </div>
   );
 };
 
