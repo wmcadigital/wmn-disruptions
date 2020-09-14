c8c658105c54bcb84891eab6e9ae514459180bd3
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-09-14 14:55:07 +0100

    Split handleKeyDown event to own hook.

--- src/components/shared/Tray/TrayComponents/AutoComplete/BusAutocomplete/BusAutoComplete.js
+++ src/components/shared/Tray/TrayComponents/AutoComplete/BusAutocomplete/BusAutoComplete.js
@@ -7,6 +7,8 @@ import useResetState from 'customHooks/useResetState';
 import Message from 'components/shared/Message/Message';
 import Icon from 'components/shared/Icon/Icon';
 import BusAutoCompleteResult from './BusAutoCompleteResult/BusAutoCompleteResult';
+// CustomHooks
+import useHandleAutoCompleteKeys from '../customHooks/useHandleAutoCompleteKeys';
 
 const BusAutoComplete = () => {
   const { updateQuery, autoCompleteState, autoCompleteDispatch } = useResetState();
@@ -14,6 +16,13 @@ const BusAutoComplete = () => {
   const [errorInfo, setErrorInfo] = useState(); // Placeholder to set error messaging
   const resultsList = useRef(null);
   const debounceInput = useRef(null);
+
+  // Import handleKeyDown function from customHook (used by all modes)
+  const { handleKeyDown } = useHandleAutoCompleteKeys(
+    resultsList,
+    DebounceInput,
+    autoCompleteState
+  );
 
   useEffect(() => {
     let mounted = true; // Set mounted to true (used later to make sure we don't do events as component is unmounting)
@@ -82,39 +91,6 @@ const BusAutoComplete = () => {
       source.cancel(); // cancel the request
     };
   }, [autoCompleteDispatch, autoCompleteState.query, autoCompleteState.selectedItem.id]);
-
-  // Function for handling keyboard/keydown events (controls the up/down arrow on autocomplete results)
-  const handleKeyDown = ({ keyCode, target }) => {
-    // Keycodes:
-    // 40 = down arrow
-    // 38 = up arrow
-    // 13 = enter
-    // 32 = space
-
-    // If down arrow pressed and current target is input (we are still in autocomplete debounce) and there are results
-    if (target.localName === 'input') {
-      if (keyCode === 40 && autoCompleteState.data.length) {
-        resultsList.current.firstChild.focus(); // Then focus on the first child in results list
-      }
-    } else {
-      // If down arrow and there is a next sibling/result
-      if (keyCode === 40 && target.nextSibling) {
-        target.nextSibling.focus(); // Then focus on next sibling/result
-      }
-      // Else if up arrow and there is a prev sibling/result
-      else if (keyCode === 38 && target.previousSibling) {
-        target.previousSibling.focus(); // Then focus on prev sibling/result
-      }
-      // Else if up arrow and no previous sibling
-      else if (keyCode === 38) {
-        debounceInput.current.focus(); // Then focus back on autoComplete input
-      }
-      // If enter or space pressed
-      if (keyCode === 13 || keyCode === 32) {
-        target.click(); // then emulate click event (select it)
-      }
-    }
-  };
 
   return (
     <>
