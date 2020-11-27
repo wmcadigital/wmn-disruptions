c9e6a6d82ce7996dfe889e27ee19b064030a7c06
Gilad Grant <ggrant966@gmail.com>
2020-11-27 16:14:25 +0000

    add error handling to api call

--- src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/TramInfoAboutSelectedService/TramInfoAboutSelectedService.js
+++ src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/TramInfoAboutSelectedService/TramInfoAboutSelectedService.js
@@ -1,11 +1,19 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AutoCompleteContext } from 'globalState';
 import axios from 'axios';
+// Components
 import FavBtn from 'components/shared/FavBtn/FavBtn';
+import Message from 'components/shared/Message/Message';
+// Local
+import numberToWord from '../helpers/numberToWord';
 
 const TramInfoAboutSelectedService = () => {
   const [autoCompleteState, autoCompleteDispatch] = useContext(AutoCompleteContext);
   const { selectedItem, selectedItemTo } = autoCompleteState;
+  const [state, setState] = useState({
+    loading: false,
+    error: false,
+  });
 
   // Placeholder variables
   let currentItem;
@@ -22,6 +30,7 @@ const TramInfoAboutSelectedService = () => {
     if (bothItemsSelected) {
       const source = axios.CancelToken.source(); // Set source of cancelToken
       const { REACT_APP_API_HOST, REACT_APP_API_KEY } = process.env; // Destructure env vars
+      setState({ loading: true, error: false });
       // Fetch the stops between
       axios
         .get(
@@ -35,20 +44,27 @@ const TramInfoAboutSelectedService = () => {
         )
         .then((response) => {
           // Update selectedItem.lines with inbetween lines
-          // API returns the stops in the opposite order to the user's input so needs to be reversed
-          const lines = response.data?.data.reverse();
-
+          // to match the user's input
+          const lines = response.data?.data;
+          setState({ loading: false, error: false });
           if (lines) {
             autoCompleteDispatch({
               type: 'UDPATE_SELECTED_ITEM_LINES',
-              payload: lines,
+              // API returns the stops in a certain direction so we check the first element to see if it must be reversed
+              payload: lines[0].atcoCode === selectedItem.id ? lines : lines.reverse(),
             });
           }
         })
         .catch((error) => {
           if (!axios.isCancel(error)) {
             // Update error message
-            // TODO: Add error message
+            setState({
+              loading: false,
+              error: {
+                title: 'Please try again',
+                message: 'Apologies, we are having technical difficulties.',
+              },
+            });
             // eslint-disable-next-line no-console
             console.log({ error });
           }
@@ -56,44 +72,54 @@ const TramInfoAboutSelectedService = () => {
     }
   }, [autoCompleteDispatch, bothItemsSelected, selectedItem.id, selectedItemTo.id]);
 
-  return (
+  // Info to show for only one item selected
+  if (!bothItemsSelected) {
+    return (
+      <>
+        <p>
+          Press star icon to save the <strong>{currentItem.stopName}</strong> stop to your
+          favourites
+        </p>
+        <FavBtn
+          id={currentItem.id}
+          text={currentItem.stopName}
+          title={currentItem.id}
+          mode="tram"
+          narrow
+        />
+      </>
+    );
+  }
+
+  if (state.error) {
+    const { error } = state;
+    return <Message type="error" title={error.title} message={error.message} />;
+  }
+
+  return state.loading || !selectedItem.lines ? (
+    <div className="wmnds-loader" role="alert" aria-live="assertive">
+      <p className="wmnds-loader__content">Content is loading...</p>
+    </div>
+  ) : (
     <>
-      {/* Update the info depending on how many stops are selected */}
-      {!bothItemsSelected ? (
-        <>
-          <p>
-            Press star icon to save the <strong>{currentItem.stopName}</strong> stop to your
-            favourites
-          </p>
+      <p>
+        {numberToWord(selectedItem.lines.length)} stop{selectedItem.lines.length > 1 ? 's' : ''} are
+        available between <strong>{selectedItem.stopName}</strong> and{' '}
+        <strong>{selectedItemTo.stopName}</strong> stops.
+      </p>
+      <p>Press star icon to save a stop to your favourites.</p>
+      {/* Loop through lines selected and show them */}
+      {selectedItem.lines &&
+        selectedItem.lines.map(({ atcoCode, name }) => (
           <FavBtn
-            id={currentItem.id}
-            text={currentItem.stopName}
-            title={currentItem.id}
+            key={atcoCode}
+            id={atcoCode}
+            text={name}
+            title={`${name} stop`}
             mode="tram"
             narrow
           />
-        </>
-      ) : (
-        <>
-          <p>
-            Stops between <strong>{selectedItem.stopName}</strong> and{' '}
-            <strong>{selectedItemTo.stopName}</strong>
-          </p>
-          <p>Press star icon to save a stop to your favourites.</p>
-          {/* Loop through lines selected and show them */}
-          {selectedItem.lines &&
-            selectedItem.lines.map(({ atcoCode, name }) => (
-              <FavBtn
-                key={atcoCode}
-                id={atcoCode}
-                text={name}
-                title={atcoCode}
-                mode="tram"
-                narrow
-              />
-            ))}
-        </>
-      )}
+        ))}
     </>
   );
 };
