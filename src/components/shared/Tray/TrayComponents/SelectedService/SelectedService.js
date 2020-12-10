71d53f9c983b7fd550c37f8e6212710fd477d069
Gilad Grant <ggrant966@gmail.com>
2020-12-10 15:15:12 +0000

    move child component toggling logic to custom hook

--- src/components/shared/Tray/TrayComponents/SelectedService/SelectedService.js
+++ src/components/shared/Tray/TrayComponents/SelectedService/SelectedService.js
@@ -1,72 +1,32 @@
-import React, { useContext } from 'react';
-import { FetchDisruptionsContext, AutoCompleteContext, ModeContext } from 'globalState';
+import React from 'react';
 // Import customHooks
-import useFilterLogic from 'customHooks/useFilterLogic';
-// Imported components
 import Message from 'components/shared/Message/Message';
+import useShowSelectedServicesInfo from './customHooks/useShowSelectedServiceInfo';
+// Imported components
 import DisruptedService from './DisruptedService/DisruptedService';
 import InfoAboutSelectedService from './InfoAboutSelectedService/InfoAboutSelectedService';
 import SaveRoutesMessage from './SaveRoutesMessage/SaveRoutesMessage';
 
 const SelectedService = () => {
-  const [fetchDisruptionsState] = useContext(FetchDisruptionsContext);
-  const [autoCompleteState] = useContext(AutoCompleteContext);
-  const [modeState] = useContext(ModeContext);
-  // The below will check all disruptions and will return any disruption where the mode is bus and the id the user clicked in the autocomplete is within the servicesAffected array
-  const selectedData = useFilterLogic();
-
-  // Variables used to toggle the display of each element
-  const isModeSelected = Object.keys(modeState).length !== 0 && modeState.mode !== null;
-  // Toggle display of SaveRoutesMessage and InfoAboutSelectedService
-  const showSelectedServiceInfo =
-    autoCompleteState.selectedItem.severity && !autoCompleteState.selectedItem.selectedByMap;
-
-  // Toggle display of 'Good Service' message
-  const showMessage = (() => {
-    // If no mode has been selected or if there are disruptions on the map are  then don't show anything
-    if (!isModeSelected || selectedData.length || !fetchDisruptionsState.isMapVisible) {
-      return false;
-    }
-
-    return true;
-  })();
-
-  // Toggle display of filtered disrupted services
-  const showDisruptedServices = (() => {
-    if (!isModeSelected || selectedData.length < 1 || !fetchDisruptionsState.isMapVisible) {
-      return false;
-    }
-
-    switch (modeState.mode) {
-      case 'bus':
-        return !!autoCompleteState.selectedItem.id;
-
-      case 'train':
-        return autoCompleteState.selectedItem.id && autoCompleteState.selectedItemTo.id;
-
-      case 'tram':
-        return autoCompleteState.selectedItem.id && autoCompleteState.selectedItemTo.id;
-
-      default:
-        return true;
-    }
-  })();
+  const {
+    showInfoAboutSelectedService,
+    showServiceMessage,
+    showDisruptedServices,
+    showLineBreak,
+    disruptedServices,
+  } = useShowSelectedServicesInfo();
 
   return (
     <>
       {/* If ann item has been selected that is not by the map then show selected services that you can fav and also show save message box. selectedItem.severity is used as the if statement to ensure all other data has been mapped to the state, using the id would cause an error as this can be gotten from the URL */}
-      {showSelectedServiceInfo && (
-        <>
-          <hr className="wmnds-col-1" />
-          <SaveRoutesMessage />
-          <InfoAboutSelectedService />
-        </>
-      )}
+      {showLineBreak && <hr className="wmnds-col-1" />}
+      <SaveRoutesMessage />
+      {showInfoAboutSelectedService && <InfoAboutSelectedService />}
       {/* If no selectedData then it must be good service */}
-      {showMessage && <Message />}
+      {showServiceMessage && <Message />}
       {/* If there are selectedData then there must be disruptions, loop through */}
       {showDisruptedServices &&
-        selectedData.map((disruption) => (
+        disruptedServices.map((disruption) => (
           <DisruptedService disruption={disruption} key={disruption.id} />
         ))}
     </>
---
+++ src/components/shared/Tray/TrayComponents/SelectedService/customHooks/useShowSelectedServiceInfo.js
@@ -0,0 +1,88 @@
+import { useContext } from 'react';
+import { FetchDisruptionsContext, AutoCompleteContext, ModeContext } from 'globalState';
+// Import customHooks
+import useFilterLogic from 'customHooks/useFilterLogic';
+
+const useShowSelectedServiceInfo = () => {
+  const [fetchDisruptionsState] = useContext(FetchDisruptionsContext);
+  const [autoCompleteState] = useContext(AutoCompleteContext);
+  const { selectedItem, selectedItemTo } = autoCompleteState;
+  const [modeState] = useContext(ModeContext);
+  // The below will check all disruptions and will return any disruption where the mode is bus and the id the user clicked in the autocomplete is within the servicesAffected array
+  const disruptedServices = useFilterLogic();
+  // Create boolean variables to use in comparisons
+  const isModeSelected = Object.keys(modeState).length !== 0 && modeState.mode !== null;
+  const anyDisruptionsToShow = disruptedServices.length > 0;
+  // Below creates an object that shows the state of the autoComplete inputs per mode.
+  const selectedItemsAre = (() => {
+    if (!isModeSelected) {
+      return {
+        allEmpty: true,
+        allSelected: false,
+      };
+    }
+
+    switch (modeState.mode) {
+      // Bus only has one autoComplete, so we just check if it's filled or not
+      case 'bus':
+        return {
+          allEmpty: !selectedItem.id,
+          allSelected: !!selectedItem.id,
+        };
+      // Train has two inputs, so both must be filled or neither
+      case 'train':
+        return {
+          allEmpty: !selectedItem.id && !selectedItemTo.id,
+          allSelected: selectedItem.id && selectedItemTo.id,
+        };
+      // Tram is like train, but filling in both triggers a sideEffect (fetching in-between stops)
+      // so we have to wait for that to resolve
+      case 'tram':
+        return {
+          allEmpty: !selectedItem.id && !selectedItemTo.id,
+          allSelected: selectedItem.id && selectedItemTo.id,
+          doneWithSideEffects:
+            selectedItem.id === selectedItemTo.id ||
+            (selectedItem.lines !== undefined && selectedItem.lines.length > 0),
+        };
+
+      default:
+        return {
+          allEmpty: true,
+          allSelected: false,
+        };
+    }
+  })();
+  const { isMapVisible } = fetchDisruptionsState;
+  const { selectedByMap } = selectedItem;
+
+  // Variables to toggle the visibility of SelectedService child components
+  const showInfoAboutSelectedService =
+    !selectedByMap && isModeSelected && selectedItemsAre.allSelected;
+
+  const showServiceMessage =
+    isMapVisible &&
+    isModeSelected &&
+    (selectedItemsAre.allEmpty ||
+      (selectedItemsAre.doneWithSideEffects === undefined && selectedItemsAre.allSelected) ||
+      selectedItemsAre.doneWithSideEffects) &&
+    !anyDisruptionsToShow;
+
+  const showDisruptedServices =
+    isMapVisible &&
+    isModeSelected &&
+    anyDisruptionsToShow &&
+    (selectedItemsAre.allSelected || selectedByMap);
+
+  const showLineBreak = showInfoAboutSelectedService && showServiceMessage;
+
+  return {
+    showInfoAboutSelectedService,
+    showServiceMessage,
+    showDisruptedServices,
+    showLineBreak,
+    disruptedServices,
+  };
+};
+
+export default useShowSelectedServiceInfo;
