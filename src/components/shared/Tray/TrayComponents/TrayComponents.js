1c463689ebfcc26b7842107b5c153a1202c78346
Gil <43111519+gldgrnt@users.noreply.github.com>
2020-12-15 10:35:55 +0000

    fix 'Good Service' message not showing / showing incorrectly (#426)
    * move selectedService logic to the component
    
    * add empty lines array when selectedItem is tram
    
    * move child component toggling logic to custom hook
    
    * change var name to 'areSelectedItems' for clarity
    
    * refactor for clarity
    
    * refactor and simplify further
    
    * only show SaveRoutesMessage with search results
    
    * change empty and selected requirements for bus
    
    Co-authored-by: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>

--- src/components/shared/Tray/TrayComponents/TrayComponents.js
+++ src/components/shared/Tray/TrayComponents/TrayComponents.js
@@ -11,19 +11,7 @@ import SelectedService from './SelectedService/SelectedService';
 import s from './TrayComponents.module.scss';
 
 const TrayComponents = () => {
-  const { autoCompleteState, modeState, resetTray } = useResetState(); // Get reset methods and state from custom hook
-
-  let showSelectedService = false;
-  // If there is a selectedItem from a map click or the user has selected train / tram (we need both the from/to Ids) or if any other mode we only need one id...
-  if (autoCompleteState.selectedItem.selectedByMap) {
-    showSelectedService = true;
-  } else if (modeState.mode === 'train' || modeState.mode === 'tram') {
-    if (autoCompleteState.selectedItem.id && autoCompleteState.selectedItemTo.id) {
-      showSelectedService = true;
-    }
-  } else if (autoCompleteState.selectedItem.id) {
-    showSelectedService = true;
-  }
+  const { resetTray } = useResetState(); // Get reset methods and state from custom hook
 
   return (
     <>
@@ -44,7 +32,7 @@ const TrayComponents = () => {
 
       <AutoComplete />
 
-      {showSelectedService && <SelectedService />}
+      <SelectedService />
     </>
   );
 };
