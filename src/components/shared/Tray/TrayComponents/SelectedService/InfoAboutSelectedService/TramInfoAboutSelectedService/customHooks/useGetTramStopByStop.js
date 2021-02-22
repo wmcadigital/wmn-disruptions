65ce44cd63908ae8b534b0c7aba57ce2066e6293
Gilad Grant <ggrant966@gmail.com>
2021-02-22 15:02:16 +0000

    refactor to be like useAutoCompleteAPI

--- src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/TramInfoAboutSelectedService/customHooks/useGetTramStopByStop.js
+++ src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/TramInfoAboutSelectedService/customHooks/useGetTramStopByStop.js
@@ -1,68 +1,99 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useCallback, useRef } from 'react';
 import axios from 'axios';
 import { AutoCompleteContext } from 'globalState';
 
 const useGetTramStopByStop = () => {
+  // State variables
   const [autoCompleteState, autoCompleteDispatch] = useContext(AutoCompleteContext);
   const { selectedItem, selectedItemTo } = autoCompleteState;
-  // Initialise state
   const [loading, setLoading] = useState(false);
-  const [errorInfo, setErrorInfo] = useState();
-
+  const [errorInfo, setErrorInfo] = useState(null);
+  // Reference variables
+  const mounted = useRef();
+  const source = useRef();
+  const apiTimeout = useRef();
+  // Helper booleans
   const bothStopsSelected = selectedItem.id && selectedItemTo.id;
   const bothStopsEqual = selectedItem.id === selectedItemTo.id;
+  // Helper functions
+  const cancelRequest = () => {
+    if (source.current) source.current.cancel('Api request timeout');
+  };
 
-  useEffect(() => {
-    if (!bothStopsSelected || bothStopsEqual) {
-      return;
+  const startApiTimeout = useCallback(() => {
+    apiTimeout.current = setTimeout(() => {
+      cancelRequest();
+    }, 15000); // 15 seconds
+  }, []);
+
+  const clearApiTimeout = () => clearTimeout(apiTimeout.current);
+
+  const handleAutoCompleteApiResponse = useCallback(
+    (response) => {
+      setLoading(false);
+      // Update selectedItem.lines with inbetween lines to match the user's input
+      const lines = response.data?.metroStopsBetween || [];
+
+      if (lines.length) {
+        autoCompleteDispatch({
+          type: 'UDPATE_SELECTED_ITEM_LINES',
+          // API returns the stops in a certain direction so we check the first element to see if it must be reversed
+          payload: lines[0].atcoCode === selectedItem.id ? lines : lines.reverse(),
+        });
+      } else {
+        setErrorInfo({
+          title: 'Please try again',
+          message: "Apologies, we couldn't load the in-between tram stops.",
+        });
+      }
+    },
+    [autoCompleteDispatch, selectedItem.id]
+  );
+
+  const handleAutoCompleteApiError = (error) => {
+    setLoading(false);
+    // Update error message
+    setErrorInfo({
+      title: 'Please try again',
+      message: 'Apologies, we are having technical difficulties.',
+    });
+    if (!axios.isCancel(error)) {
+      // eslint-disable-next-line no-console
+      console.log({ error });
     }
+  };
 
-    setLoading(true);
-    setErrorInfo();
-
-    const source = axios.CancelToken.source(); // Set source of cancelToken
+  const getInBetweenTramStops = useCallback(() => {
+    source.current = axios.CancelToken.source();
+    mounted.current = true; // Set mounted to true (used later to make sure we don't do events as component is unmounting)
     const { REACT_APP_API_HOST, REACT_APP_API_KEY } = process.env; // Destructure env vars
-
+    setLoading(true); // Update loading state to true as we are hitting API
+    startApiTimeout();
     axios
       .get(`${REACT_APP_API_HOST}/Metro/v2/stopsbetween/${selectedItem.id}/${selectedItemTo.id}`, {
         headers: {
           'Ocp-Apim-Subscription-Key': REACT_APP_API_KEY,
         },
-        cancelToken: source.token, // Set token with API call, so we can cancel this call on unmount
+        cancelToken: source.current.token, // Set token with API call, so we can cancel this call on unmount
       })
-      .then((response) => {
-        setLoading(false);
-        // Update selectedItem.lines with inbetween lines to match the user's input
-        const lines = response.data?.data;
+      .then(handleAutoCompleteApiResponse)
+      .catch(handleAutoCompleteApiError);
+  }, [handleAutoCompleteApiResponse, selectedItem.id, selectedItemTo.id, startApiTimeout]);
 
-        if (!lines) {
-          return setErrorInfo({
-            title: 'No results found',
-            message: 'Make sure you are looking for the right service, and try again.',
-          });
-        }
+  useEffect(() => {
+    if (!bothStopsSelected || bothStopsEqual) {
+      return setErrorInfo(null);
+    }
+    getInBetweenTramStops();
 
-        return autoCompleteDispatch({
-          type: 'UDPATE_SELECTED_ITEM_LINES',
-          // API returns the stops in a certain direction so we check the first element to see if it must be reversed
-          payload: lines[0].atcoCode === selectedItem.id ? lines : lines.reverse(),
-        });
-      })
-      .catch((error) => {
-        setLoading(false);
-        if (!axios.isCancel(error)) {
-          // Update error message
-          setErrorInfo({
-            title: 'Please try again',
-            message: 'Apologies, we are having technical difficulties.',
-          });
-          // eslint-disable-next-line no-console
-          console.log({ error });
-        }
-      });
-  }, [autoCompleteDispatch, bothStopsEqual, bothStopsSelected, selectedItem.id, selectedItemTo.id]);
+    return () => {
+      mounted.current = false; // Set mounted back to false on unmount
+      cancelRequest(); // cancel the request
+      clearApiTimeout(); // clear timeout
+    };
+  }, [bothStopsEqual, bothStopsSelected, getInBetweenTramStops]);
 
-  return { loading, errorInfo, autoCompleteState };
+  return { loading, errorInfo, autoCompleteState, getInBetweenTramStops };
 };
 
 export default useGetTramStopByStop;
