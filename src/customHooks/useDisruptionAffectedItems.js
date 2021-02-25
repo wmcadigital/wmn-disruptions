8989e389f05c3e9aa35340f0cfde909cb90b0a62
Gilad Grant <ggrant966@gmail.com>
2021-02-25 14:14:09 +0000

    ensure spacing is correct for show/hide button

--- src/customHooks/useDisruptionAffectedItems.js
+++ src/customHooks/useDisruptionAffectedItems.js
@@ -11,7 +11,7 @@ const useDisruptionAffectedItems = (disruption) => {
 
   // Setup showing and hiding excess disrupted servces
   const [autoCompleteState] = useContext(AutoCompleteContext);
-  const { selectedItem } = autoCompleteState;
+  const { selectedItem, selectedItemTo } = autoCompleteState;
   const { windowWidth } = useWindowHeightWidth();
   const [fetchDisruptionsState] = useContext(FetchDisruptionsContext);
   const maxShownBeforeHiding = !fetchDisruptionsState.isMapVisible && windowWidth >= 768 ? 5 : 4;
@@ -55,59 +55,128 @@ const useDisruptionAffectedItems = (disruption) => {
     </>
   );
 
-  const affectedItems = () => (
-    <>
-      {/* Affected */}
-      <div className="wmnds-col-1 ">
-        <strong>Affected {whatIsAffected}:</strong>
-      </div>
-      <div className="wmnds-col-1">
-        {/* Affected Services / Bus */}
-        {disruption.mode === 'bus' && disruption?.servicesAffected && (
-          <>
-            {disruption.servicesAffected
-              .sort(
-                (a, b) => a.serviceNumber.replace(/\D/g, '') - b.serviceNumber.replace(/\D/g, '')
-              )
-              // Sort again to move a user's selected item to the front so it's not "hidden" behind the button
-              .sort((a, b) => {
-                if (a.serviceNumber === selectedItem?.serviceNumber) return -1;
-                return b.serviceNumber === selectedItem?.serviceNumber ? 1 : 0;
-              })
-              .slice(0, sliceUpper)
-              .map((affected) => (
-                <FavBtn
-                  id={affected.id}
-                  severity={disruption.disruptionSeverity}
-                  text={affected.serviceNumber}
-                  title={`${affected.routeDescriptions[0].description} (${affected.operatorName})`}
-                  mode={disruption.mode}
-                  key={affected.id}
+  const affectedItems = () =>
+    (disruption?.servicesAffected && disruption.servicesAffected.length) ||
+    (disruption?.stopsAffected && disruption.stopsAffected.length) ? (
+      <div className={fetchDisruptionsState.isMapVisible ? '' : 'wmnds-p-l-md'}>
+        {/* Affected */}
+        <div className="wmnds-col-1">
+          <strong>Affected {whatIsAffected}:</strong>
+        </div>
+        <div className="wmnds-col-1">
+          {/* Affected Services / Bus */}
+          {disruption.mode === 'bus' && disruption?.servicesAffected && (
+            <>
+              {disruption.servicesAffected
+                .sort(
+                  (a, b) => a.serviceNumber.replace(/\D/g, '') - b.serviceNumber.replace(/\D/g, '')
+                )
+                // Sort again to move a user's selected item to the front so it's not "hidden" behind the button
+                .sort((a, b) => {
+                  if (a.serviceNumber === selectedItem?.serviceNumber) return -1;
+                  return b.serviceNumber === selectedItem?.serviceNumber ? 1 : 0;
+                })
+                .slice(0, sliceUpper)
+                .map((affected) => (
+                  <FavBtn
+                    id={affected.id}
+                    severity={disruption.disruptionSeverity}
+                    text={affected.serviceNumber}
+                    title={`${affected.routeDescriptions[0].description} (${affected.operatorName})`}
+                    mode={disruption.mode}
+                    key={affected.id}
+                  />
+                ))}
+              {disruption.servicesAffected.length > maxShownBeforeHiding && (
+                <ToggleMoreAffectedItems
+                  handleClick={toggleExpanded}
+                  id={`toggleMoreAffectedItems_${disruption.id}`}
+                  isExpanded={isExpanded}
+                  amountHidden={disruption.servicesAffected.length - maxShownBeforeHiding}
+                  serviceText={
+                    disruption.servicesAffected.length - maxShownBeforeHiding > 1
+                      ? whatIsAffected
+                      : whatIsAffectedSingular
+                  }
                 />
-              ))}
-            {disruption.servicesAffected.length > maxShownBeforeHiding && (
-              <ToggleMoreAffectedItems
-                handleClick={toggleExpanded}
-                id={`toggleMoreAffectedItems_${disruption.id}`}
-                isExpanded={isExpanded}
-                amountHidden={disruption.servicesAffected.length - maxShownBeforeHiding}
-                serviceText={
-                  disruption.servicesAffected.length - maxShownBeforeHiding > 1
-                    ? whatIsAffected
-                    : whatIsAffectedSingular
-                }
-              />
+              )}
+            </>
+          )}
+          {/* Affected stops / Tram */}
+          {disruption.mode === 'tram' && disruption?.servicesAffected && disruption?.stopsAffected && (
+            <>
+              {disruption.stopsAffected
+                .sort((a, b) => {
+                  // Convert stop name text to lowercase
+                  const x = a.name.toLowerCase();
+                  const y = b.name.toLowerCase();
+                  // Return minus or plus values when comparing prev/next string. This ensures alphabetical sorting.
+                  if (x < y) return -1;
+                  if (x > y) return 1;
+                  return 0;
+                })
+                // Sort again to move a user's selected items to the front so it's not "hidden" behind the button
+                .sort((a, b) => {
+                  const atcoCodeA = a.atcoCode.replace('940G', '9400');
+                  const atcoCodeB = b.atcoCode.replace('940G', '9400');
+                  if (atcoCodeA === selectedItem?.id || atcoCodeA === selectedItemTo?.id) return -1;
+                  return atcoCodeB === selectedItem?.id || atcoCodeB === selectedItemTo?.id ? 1 : 0;
+                })
+                .slice(0, sliceUpper)
+                .map((affected) => (
+                  <FavBtn
+                    id={affected.atcoCode.replace('940G', '9400')}
+                    severity={disruption.disruptionSeverity}
+                    text={affected.name.replace(' (Midland Metro Stop)', '')}
+                    title={disruption?.servicesAffected[0]?.routeDescriptions[0]?.description}
+                    mode={disruption.mode}
+                    key={affected.atcoCode}
+                    narrow
+                  />
+                ))}
+              <div className="wmnds-m-t-md">
+                {disruption.stopsAffected.length > maxShownBeforeHiding && (
+                  <ToggleMoreAffectedItems
+                    handleClick={toggleExpanded}
+                    id={`toggleMoreAffectedItems_${disruption.id}`}
+                    isExpanded={isExpanded}
+                    amountHidden={disruption.stopsAffected.length - maxShownBeforeHiding}
+                    serviceText={
+                      disruption.stopsAffected.length - maxShownBeforeHiding > 1
+                        ? whatIsAffected
+                        : whatIsAffectedSingular
+                    }
+                  />
+                )}
+              </div>
+            </>
+          )}
+          {/* Affected services / Tram */}
+          {disruption.mode === 'tram' &&
+            !disruption?.stopsAffected &&
+            disruption?.servicesAffected && (
+              <div className="wmnds-m-b-md">
+                {disruption.servicesAffected.slice(0, sliceUpper).map((affected) => (
+                  <FavBtn
+                    id={affected.id}
+                    severity={disruption.disruptionSeverity}
+                    text="Birmingham to Wolverhampton"
+                    title={disruption?.servicesAffected[0]?.routeDescriptions[0]?.description}
+                    mode={disruption.mode}
+                    key={affected.id}
+                    narrow
+                  />
+                ))}
+              </div>
             )}
-          </>
-        )}
-        {/* Affected stops / Tram */}
-        {disruption.mode === 'tram' && disruption?.servicesAffected && disruption?.stopsAffected && (
-          <>
-            {disruption.stopsAffected
+          {/* Affected Stations / Train */}
+          {disruption.mode === 'train' &&
+            disruption?.servicesAffected[0]?.routeDescriptions &&
+            disruption.servicesAffected[0].routeDescriptions
               .sort((a, b) => {
-                // Convert stop name text to lowercase
-                const x = a.name.toLowerCase();
-                const y = b.name.toLowerCase();
+                // Convert line name text to lowercase
+                const x = a.description.toLowerCase();
+                const y = b.description.toLowerCase();
                 // Return minus or plus values when comparing prev/next string. This ensures alphabetical sorting.
                 if (x < y) return -1;
                 if (x > y) return 1;
@@ -116,72 +185,19 @@ const useDisruptionAffectedItems = (disruption) => {
               .slice(0, sliceUpper)
               .map((affected) => (
                 <FavBtn
-                  id={affected.atcoCode}
+                  id={affected.description}
                   severity={disruption.disruptionSeverity}
-                  text={affected.name.replace(' (Midland Metro Stop)', '')}
-                  title={disruption?.servicesAffected[0]?.routeDescriptions[0]?.description}
+                  text={affected.description}
+                  title={affected.description}
                   mode={disruption.mode}
-                  key={affected.atcoCode}
+                  key={affected.id}
                 />
               ))}
-            {disruption.stopsAffected.length > maxShownBeforeHiding && (
-              <ToggleMoreAffectedItems
-                handleClick={toggleExpanded}
-                id={`toggleMoreAffectedItems_${disruption.id}`}
-                isExpanded={isExpanded}
-                amountHidden={disruption.stopsAffected.length - maxShownBeforeHiding}
-                serviceText={
-                  disruption.stopsAffected.length - maxShownBeforeHiding > 1
-                    ? whatIsAffected
-                    : whatIsAffectedSingular
-                }
-              />
-            )}
-          </>
-        )}
-        {/* Affected services / Tram */}
-        {disruption.mode === 'tram' &&
-          !disruption?.stopsAffected &&
-          disruption?.servicesAffected &&
-          disruption.servicesAffected
-            .slice(0, sliceUpper)
-            .map((affected) => (
-              <FavBtn
-                id={affected.id}
-                severity={disruption.disruptionSeverity}
-                text={`${affected.operatorName} - ${affected?.routeDescriptions[0]?.description}`}
-                title={disruption?.servicesAffected[0]?.routeDescriptions[0]?.description}
-                mode={disruption.mode}
-                key={affected.id}
-              />
-            ))}
-        {/* Affected Stations / Train */}
-        {disruption.mode === 'train' &&
-          disruption?.servicesAffected[0]?.routeDescriptions &&
-          disruption.servicesAffected[0].routeDescriptions
-            .sort((a, b) => {
-              // Convert line name text to lowercase
-              const x = a.description.toLowerCase();
-              const y = b.description.toLowerCase();
-              // Return minus or plus values when comparing prev/next string. This ensures alphabetical sorting.
-              if (x < y) return -1;
-              if (x > y) return 1;
-              return 0;
-            })
-            .slice(0, sliceUpper)
-            .map((affected) => (
-              <FavBtn
-                id={affected.description}
-                severity={disruption.disruptionSeverity}
-                text={affected.description}
-                title={affected.description}
-                mode={disruption.mode}
-                key={affected.id}
-              />
-            ))}
+        </div>
       </div>
-    </>
-  );
+    ) : (
+      <div />
+    );
 
   return { iconLeft, title, affectedItems };
 };
