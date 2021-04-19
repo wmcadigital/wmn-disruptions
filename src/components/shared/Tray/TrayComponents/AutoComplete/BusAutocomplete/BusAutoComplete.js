c8c8d898103b77990fdc735c8cd88e342ca37cbe
Gilad Grant <ggrant966@gmail.com>
2021-04-19 14:25:59 +0100

    update api path

--- src/components/shared/Tray/TrayComponents/AutoComplete/BusAutocomplete/BusAutoComplete.js
+++ src/components/shared/Tray/TrayComponents/AutoComplete/BusAutocomplete/BusAutoComplete.js
@@ -18,7 +18,7 @@ const BusAutoComplete = () => {
   const debounceInput = useRef(null);
 
   const { loading, errorInfo, results, getAutoCompleteResults } = useAutoCompleteAPI(
-    `/bus/v1/service?q=${encodeURI(autoCompleteState.query)}`,
+    `/lineinfo?q=${encodeURI(autoCompleteState.query)}`,
     'bus',
     autoCompleteState.query
   );
