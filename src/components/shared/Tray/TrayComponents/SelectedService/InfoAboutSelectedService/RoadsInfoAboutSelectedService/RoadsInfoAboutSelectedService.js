e404a9da938693b1a332e89d864ef562d553f6ac
Gilad Grant <ggrant966@gmail.com>
2021-05-20 01:49:59 +0100

    add correct styles

--- src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/RoadsInfoAboutSelectedService/RoadsInfoAboutSelectedService.js
+++ src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/RoadsInfoAboutSelectedService/RoadsInfoAboutSelectedService.js
@@ -4,24 +4,24 @@ import FavBtn from 'components/shared/FavBtn/FavBtn';
 
 const RoadsInfoAboutSelectedService = () => {
   const [autoCompleteState] = useContext(AutoCompleteContext);
-  const { selectedItem } = autoCompleteState;
-
-  // Make any letters in the service name uppercase
-  const service = selectedItem.serviceNumber.toUpperCase();
+  const { selectedLocation, selectedItem } = autoCompleteState;
+  const { address, radius, lat, lon } = selectedLocation;
+  const encodedId = encodeURI(`${address};${lat};${lon};${radius}`); // use semi-colons to sepearate as 'address' field may contain commas
 
   return (
     <>
-      <p>
-        Press star icon to save service <strong>{service}</strong> to your favourites
-      </p>
-
-      <FavBtn
-        id={selectedItem.id}
-        text={service}
-        title={`${selectedItem.routeName} (${selectedItem.operator})`}
-        mode="roads"
-        narrow
-      />
+      <div className="wmnds-m-t-md">
+        <span>
+          Press star icon to save selected road area to your favourites{' '}
+          <strong>{`${address} + ${radius} miles`}</strong>
+        </span>
+        <FavBtn id={encodedId} text="" title={`${address} + ${radius} miles`} mode="roads" inline />
+      </div>
+      {!selectedItem.selectedByMap && (
+        <div className="wmnds-m-t-sm">
+          <hr className="wmnds-col-1" />
+        </div>
+      )}
     </>
   );
 };
