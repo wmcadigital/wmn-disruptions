3c3c994de295451031d876782c25cbc0c58c5baa
Gilad Grant <ggrant966@gmail.com>
2021-06-24 11:38:50 +0100

    update train favourites to include more info

--- src/components/shared/FavBtn/FavBtn.js
+++ src/components/shared/FavBtn/FavBtn.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 // Import contexts
 import { FavsContext } from 'globalState';
@@ -10,12 +10,28 @@ import s from './FavBtn.module.scss';
 
 const FavBtn = ({ id, severity, text, title, mode, narrow, inline }) => {
   const [favState, favDispatch] = useContext(FavsContext); // Get fav state from globalState
-  const [isFav, setIsFav] = useState(favState.favs[mode].indexOf(id) > -1); // Check favs on load to see if ours is included
+
+  const isIdFavourited = useCallback(
+    (favId) => {
+      if (mode !== 'train') return favState.favs[mode].indexOf(favId) > -1;
+      if (typeof favId === 'string') {
+        favDispatch({ type: 'REMOVE_FAV', favId, mode });
+        return false;
+      }
+      return favState.favs[mode].some(
+        (trainFav) =>
+          trainFav.to === favId.to && trainFav.from === favId.from && trainFav.line === favId.line
+      );
+    },
+    [favDispatch, favState.favs, mode]
+  );
+
+  const [isFav, setIsFav] = useState(isIdFavourited(id)); // Check favs on load to see if ours is included
 
   // UseEffect to watch for changes of favState, then we can reload component with new favourites
   useEffect(() => {
-    setIsFav(favState.favs[mode].indexOf(id) > -1); // Check reloaded favs to see if our id is included in there
-  }, [favState.favs, id, mode]);
+    setIsFav(isIdFavourited(id)); // Check reloaded favs to see if our id is included in there
+  }, [favState.favs, id, isIdFavourited, mode]);
 
   const toggleFav = () => {
     setIsFav(!isFav); // Toggle the fav state
@@ -58,7 +74,14 @@ const FavBtn = ({ id, severity, text, title, mode, narrow, inline }) => {
 
 // Set props
 FavBtn.propTypes = {
-  id: PropTypes.string, // button type, by default it is type="button"
+  id: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      to: PropTypes.string.isRequired,
+      from: PropTypes.string.isRequired,
+      line: PropTypes.string.isRequired,
+    }),
+  ]), // button type, by default it is type="button"
   mode: PropTypes.string, // Mode type
   narrow: PropTypes.bool,
   inline: PropTypes.bool,
