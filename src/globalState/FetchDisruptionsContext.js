a975b1aed6bd3af45e05b3b7311d6d9e63491f34
Gilad Grant <ggrant966@gmail.com>
2021-06-25 09:39:27 +0100

    fix empty localStorage bug

--- src/globalState/FetchDisruptionsContext.js
+++ src/globalState/FetchDisruptionsContext.js
@@ -9,7 +9,7 @@ export const FetchDisruptionsProvider = (props) => {
 
   const [fetchDisruptionsState, setFetchDisruptionsState] = useState({
     isMapVisible:
-      JSON.parse(localStorage.getItem('disruptionsApp')).isMapVisible !== undefined
+      JSON.parse(localStorage.getItem('disruptionsApp'))?.isMapVisible !== undefined
         ? JSON.parse(localStorage.getItem('disruptionsApp')).isMapVisible // get the user's preference from localStorage
         : getSearchParam('isMapVisible') === 'true', // store map visible. If the URL doesn't contain param 'isMapVisible' then it must be false (bool) else it's true (bool). Written like this as the url is a string and we need a bool here. This is set in Header component.
     data: [], // used to store data
