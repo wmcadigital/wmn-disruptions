4bb59685904881db55729ea675c2bdf5ef5518cf
Gilad Grant <ggrant966@gmail.com>
2021-07-19 11:13:19 +0100

    change favBtn id to selectedService

--- src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/RoadsInfoAboutSelectedService/RoadsInfoAboutSelectedService.js
+++ src/components/shared/Tray/TrayComponents/SelectedService/InfoAboutSelectedService/RoadsInfoAboutSelectedService/RoadsInfoAboutSelectedService.js
@@ -5,9 +5,7 @@ import FavBtn from 'components/shared/FavBtn/FavBtn';
 const RoadsInfoAboutSelectedService = () => {
   const [autoCompleteState] = useContext(AutoCompleteContext);
   const { selectedLocation, selectedItem } = autoCompleteState;
-  const { address, radius, lat, lon } = selectedLocation;
-  // address/lat/lon/radius (may be saved as a cookie so can't be separated using ; or =)
-  const encodedId = `${encodeURI(address)}/${lat}/${lon}/${radius}`;
+  const { address, radius } = selectedLocation;
   const milesText = `mile${radius > 1 ? 's' : ''}`;
   const title = `${address} + ${radius} ${milesText}`;
 
@@ -17,7 +15,7 @@ const RoadsInfoAboutSelectedService = () => {
         <span>
           Select the star icon to add your chosen road area to the homepage <strong>{title}</strong>
         </span>
-        <FavBtn id={encodedId} text="" title={title} mode="roads" inline />
+        <FavBtn id={selectedLocation} text="" title={title} mode="roads" inline />
       </div>
       {!selectedItem.selectedByMap && (
         <div className="wmnds-m-t-sm">
