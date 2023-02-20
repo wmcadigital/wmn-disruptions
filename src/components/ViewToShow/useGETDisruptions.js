b3d18afbcfcbcd289bb9394548bb82c5bf548484
Sudheer Kotha <sudheer.kotha@wmca@org.uk>
2023-02-20 11:28:55 +0000

    remove spaces

--- src/components/ViewToShow/useGETDisruptions.js
+++ src/components/ViewToShow/useGETDisruptions.js
@@ -11,7 +11,6 @@ const useGETDisruptions = () => {
   useEffect(() => {
     const { REACT_APP_API_HOST, REACT_APP_API_KEY } = process.env; // Destructure env vars
     const randomString = Math.random().toString(36).substring(2, 15); // Generate random string
-
     axios
       .get(`${REACT_APP_API_HOST}/Disruption/v2`, {
         headers: {
