73f6e0eab19323fda7e72aae0ebb67c12fba9bc1
Sudheer Kotha <sudheer.kotha@wmca@org.uk>
2023-03-06 09:52:53 +0000

    send random string when service was unavailable

--- src/components/ViewToShow/useGETDisruptions.js
+++ src/components/ViewToShow/useGETDisruptions.js
@@ -10,35 +10,45 @@ const useGETDisruptions = () => {
 
   useEffect(() => {
     const { REACT_APP_API_HOST, REACT_APP_API_KEY } = process.env; // Destructure env vars
-    const randomString = Math.random().toString(36).substring(2, 15); // Generate random string
-    axios
-      .get(`${REACT_APP_API_HOST}/Disruption/v2`, {
-        headers: {
-          'Ocp-Apim-Subscription-Key': REACT_APP_API_KEY,
-        },
-        params: {
-          random: randomString,
-        },
-      })
-      .then((response) => {
+    const fetchData = async () => {
+      const randomString = Math.random().toString(36).substring(2, 15); // Generate random string
+      try {
+        const response = await axios.get(`${REACT_APP_API_HOST}/Disruption/v2`, {
+          headers: {
+            'Ocp-Apim-Subscription-Key': REACT_APP_API_KEY,
+          },
+          params: {
+            random: randomString,
+          },
+        });
         setFetchDisruptionsState((prevState) => ({
           ...prevState,
           data: response.data.disruptions,
         }));
-        // eslint-disable-next-line no-console
-        console.log(`${new Date().toISOString()} - randomString: ${randomString}`);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
+        if (response.status === 200) {
+          console.log(
+            `Request successful - ${new Date().toISOString()} - randomString: ${randomString}`
+          );
+        } else {
+          console.log(
+            `Service unavailable - ${new Date().toISOString()} - randomString: ${randomString}`
+          );
+        }
+        setHasError(false);
+      } catch (error) {
         console.error({ error });
+        console.log(
+          `Service unavailable - ${new Date().toISOString()} - randomString: ${randomString}`
+        );
         setHasError(true);
-      })
-      .then(() => {
-        setIsFetching(false);
-      });
+      }
+      setIsFetching(false);
+    };
+    fetchData();
+    const intervalId = setInterval(fetchData, 30000); // Call fetchData every 30 seconds
+    return () => clearInterval(intervalId);
   }, [setFetchDisruptionsState]);
 
   return { isFetching, hasError };
 };
-
 export default useGETDisruptions;
