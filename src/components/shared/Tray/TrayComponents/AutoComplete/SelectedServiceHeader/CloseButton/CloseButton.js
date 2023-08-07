b82d005521355d59894ea97c3bfd61873841eddc
Nathan Rogan <rolorogan@gmail.com>
2023-08-07 15:50:37 +0100

    fix: release build (#738)
    * update packages
    
    * fix linting issues
    
    * add babel-eslint

--- src/components/shared/Tray/TrayComponents/AutoComplete/SelectedServiceHeader/CloseButton/CloseButton.js
+++ src/components/shared/Tray/TrayComponents/AutoComplete/SelectedServiceHeader/CloseButton/CloseButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Icon from 'components/shared/Icon/Icon';
 import PropTypes from 'prop-types';
+import Icon from 'components/shared/Icon/Icon';
 
 import s from './CloseButton.module.scss';
 
