cd380dcea94ddceb14131bccf442a6ffcf88dc57
GeorgeEverett <87702223+GeorgeEverett@users.noreply.github.com>
2024-02-02 16:24:25 +0000

    Remove +1 hours and add space in comment (#742) (#743)

--- src/components/shared/DisruptionInfo/DisruptionInfo.js
+++ src/components/shared/DisruptionInfo/DisruptionInfo.js
@@ -109,13 +109,13 @@ const DisruptionInfo = ({ disruption }) => {
             <p>
               <strong>When?</strong>
               <br />
-              {/* Temporary fix for textual errors of rail disruption timings during British Summer Time */}
-              <Moment locale="en-GB" format="dddd, Do MMMM YYYY HH:mm" add={{ hours: 1 }}>
+              {/* Temporary fix for textual errors of rail disruption timings during British Summer Time:<Moment locale="en-GB" format="dddd, Do MMMM YYYY HH:mm" add={{ hours: 1 }}> */}
+              <Moment locale="en-GB" format="dddd, Do MMMM YYYY HH:mm">
                 {disruption.disruptionTimeWindow.start}
               </Moment>
               {' to '}
 
-              <Moment locale="en-GB" format="dddd, Do MMMM YYYY HH:mm" add={{ hours: 1 }}>
+              <Moment locale="en-GB" format="dddd, Do MMMM YYYY HH:mm">
                 {disruption.disruptionTimeWindow.end}
               </Moment>
             </p>
